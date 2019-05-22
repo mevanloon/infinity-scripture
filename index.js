04a3428306e85f571d9d744e01474efa3edcdd41
Matthijs <mevanloon@me.com>
2019-05-23 01:55:06 +0200

    Refactors

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 #!/usr/bin/env node
+// ./index.js 2:34-2
+// Dread it, run from it...
+// destiny arrives all the same.
+
 const parser = require('subtitles-parser')
 const fs = require('fs')
 const argv = require('minimist')(process.argv.slice(2))
 const srt = fs.readFileSync('infinity-war.srt', {encoding: 'utf8'})
 const data = parser.fromSrt(srt, true)
+const a = argv._[0]
 
-if(argv._[0]) {
-  if(argv._[0].match(/(\d{1,2}):(\d{1,2}):(\d{1,2})-(\d{1,2})/)) {
-    const fromTo = argv._[0].match(/(\d{1,2}):(\d{1,2}):(\d{1,2})-(\d{1,2})/)
-    const hoursMs = parseInt(fromTo[1])*3600000
-    const minutesMs = parseInt(fromTo[2])*60000
-    const secondsMs = parseInt(fromTo[3])*1000
-    const startMs = hoursMs + minutesMs + secondsMs
-    const numberOfLines = parseInt(fromTo[4]) || 1
-    var parsedLines = 0
+if(a && a.match(/(\d{0,2}):{0,1}(\d{1,2}):(\d{1,2})-{0,1}(\d{0,2})/)) {
+  const timeframe = a.match(/(\d{0,2}):{0,1}(\d{1,2}):(\d{1,2})-{0,1}(\d{0,2})/)
+  const hoursMs = parseInt(timeframe[1] || 0)*3600000
+  const minutesMs = parseInt(timeframe[2])*60000
+  const secondsMs = parseInt(timeframe[3])*1000
+  const startMs = hoursMs + minutesMs + secondsMs
+  const numberOfLines = parseInt(timeframe[4]) || 1
+  var parsedLines = 0
 
-    // console.log('From')
-    console.log(startMs)
-    // console.log(hoursMs, minutesMs, secondsMs)
-    // console.log(startMs)
-    // console.log(data[29].startTime)
-    // console.log(data[29].text)
-
-    for(let i=0;i<data.length;i++) {
-      if(parsedLines < numberOfLines) {
-        if(data[i].startTime > startMs) {
-          console.log(data[i].text)
-          parsedLines++
-        }
+  for(let i=0;i<data.length;i++) {
+    if(parsedLines < numberOfLines) {
+      if(data[i].startTime > startMs) {
+        console.log(data[i].text)
+        parsedLines++
       }
-      else {
-        break
-      }
-    }
-  }
-  else if(argv._[0].match(/(\d{1,2}):(\d{1,2}):(\d{1,2})/)) {
-    const startMs = ((fromTo[1]*60*60) + (fromTo[2]*60) + fromTo[3])*1000
-    console.log('From')
-    console.log(startMs)
+    } else break
   }
 }
 else {
-  console.log('argv')
+  console.log(a.match(/(\d{0,2}):{0,1}(\d{1,2}):(\d{1,2})-{0,1}(\d{0,2})/))
 }
